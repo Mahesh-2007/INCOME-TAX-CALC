dc302c9fea0effcb4e8110d150a751dbc5757d6f
Mahesh-2007 <mm2472229@gmail.com>
2024-06-04 20:23:57 +0530

    commit

--- taxCALC.java
+++ taxCALC.java
@@ -1,10 +1,10 @@
 import java.util.*;
 public class taxCALC{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner SC = new Scanner(System.in);
         
         System.out.println("PLEASE ENTER YOUR INCOME : ");
-        int INCOME = sc.nextInt();
+        int INCOME = SC.nextInt();
         if(INCOME<500000){
             System.out.println("YOR TAX IS : 0");
         }else if(INCOME>=500000 && INCOME <1000000){
